0decaa5494ee2579184e4f7ff46b7fad5b7c2656
DariushJinx <106846875+DariushJinx@users.noreply.github.com>
2023-10-07 17:23:20 +0330

    handle entity for user

---
+++ src/user/user.entity.ts
@@ -0,0 +1,31 @@
+import { hash } from 'bcrypt';
+import { BeforeInsert, Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
+
+@Entity({ name: 'users' })
+export class UserEntity {
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @Column()
+  first_name: string;
+
+  @Column()
+  last_name: string;
+
+  @Column()
+  username: string;
+
+  @Column()
+  mobile: string;
+
+  @Column()
+  email: string;
+
+  @Column()
+  password: string;
+
+  @BeforeInsert()
+  async hashPassword() {
+    this.password = await hash(this.password, 10);
+  }
+}
