2577b2dd9456e8e43ac6e74743c69881952a27bd
DariushJinx <106846875+DariushJinx@users.noreply.github.com>
2023-10-07 17:21:04 +0330

    handle orm config

---
+++ src/ormconfig.ts
@@ -0,0 +1,18 @@
+import { DataSource, DataSourceOptions } from 'typeorm';
+import { registerAs } from '@nestjs/config';
+
+const config = {
+  type: 'postgres',
+  host: 'localhost',
+  port: 5432,
+  username: 'postgres',
+  password: 'NewWorldJinx',
+  database: 'nest_backend',
+  entities: [__dirname + '/**/*.entity{.ts,.js}'],
+  migrations: [__dirname + '/migration/*{.ts,.js}'],
+  synchronize: false,
+  migrationsRun: true,
+};
+
+export default registerAs('typeorm', () => config);
+export const connectionSource = new DataSource(config as DataSourceOptions);
