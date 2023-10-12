acec2848d1bd05067e31a2cca5e3a3b1e715f043
DariushJinx <106846875+DariushJinx@users.noreply.github.com>
2023-10-12 13:00:32 +0330

    file updated with login user and handle count role

--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -3,6 +3,11 @@ import { InjectRepository } from '@nestjs/typeorm';
 import { UserEntity } from './user.entity';
 import { Repository } from 'typeorm';
 import { RegisterDto } from './dto/register.dto';
+import { LoginDto } from './dto/login.dto';
+import { compare } from 'bcrypt';
+import { sign } from 'jsonwebtoken';
+import { ACCESS_TOKEN_SECRET_KEY } from 'src/config';
+import { UserResponseInterface } from './types/userResponse.interface';
 
 @Injectable()
 export class UserService {
@@ -62,7 +67,66 @@ export class UserService {
     }
 
     const newUser = new UserEntity();
+    const userCounts = await this.userRepository.count();
+    newUser.role = userCounts > 3 ? 'USER' : 'ADMIN';
     Object.assign(newUser, registerDto);
     return await this.userRepository.save(newUser);
+  }
+
+  async loginUser(loginDto: LoginDto): Promise<UserEntity> {
+    const errorResponse = {
+      errors: {},
+    };
+
+    const user = await this.userRepository.findOne({
+      select: {
+        password: true,
+        email: true,
+      },
+      where: {
+        email: loginDto.email,
+      },
+    });
+
+    if (!user) {
+      errorResponse.errors['user'] =
+        'ایمیل و یا رمز عبور وارد شده صحیح نمی باشد';
+      throw new HttpException(errorResponse, HttpStatus.UNPROCESSABLE_ENTITY);
+    }
+
+    const isPasswordCorrect = await compare(loginDto.password, user.password);
+
+    if (!isPasswordCorrect) {
+      errorResponse.errors['user'] =
+        'ایمیل و یا رمز عبور وارد شده صحیح نمی باشد';
+      throw new HttpException(errorResponse, HttpStatus.UNPROCESSABLE_ENTITY);
+    }
+
+    delete user.password;
+    return user;
+  }
+
+  generateJwtToken(user: UserEntity): string {
+    const options = {
+      expiresIn: '30d',
+    };
+    return sign(
+      {
+        id: user.id,
+        username: user.username,
+        email: user.email,
+      },
+      ACCESS_TOKEN_SECRET_KEY,
+      options,
+    );
+  }
+
+  async buildUserResponse(user: UserEntity): Promise<UserResponseInterface> {
+    return {
+      user: {
+        ...user,
+        token: this.generateJwtToken(user),
+      },
+    };
   }
 }
