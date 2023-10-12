19c3bd388799b1bd3a3f0890bbac370adce48c1e
DariushJinx <106846875+DariushJinx@users.noreply.github.com>
2023-10-12 12:58:40 +0330

    file updated with dto and interface

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -3,6 +3,8 @@ import { UserService } from './user.service';
 import { RegisterDto } from './dto/register.dto';
 import { UserType } from './types/user.types';
 import { BackendValidationPipe } from 'src/shared/pipes/backendValidation.pipe';
+import { LoginDto } from './dto/login.dto';
+import { UserResponseInterface } from './types/userResponse.interface';
 
 @Controller('auth')
 export class UserController {
@@ -11,8 +13,14 @@ export class UserController {
   @Post('register')
   @UsePipes(new BackendValidationPipe())
   async registerUser(@Body() registerDto: RegisterDto): Promise<UserType> {
-    console.log('registerDto : ', registerDto);
     const user = await this.userService.registerUser(registerDto);
     return user;
+  }
+
+  @Post('login')
+  @UsePipes(new BackendValidationPipe())
+  async loginUser(@Body() loginDto: LoginDto): Promise<UserResponseInterface> {
+    const user = await this.userService.loginUser(loginDto);
+    return await this.userService.buildUserResponse(user);
   }
 }
