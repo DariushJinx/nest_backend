f91511446ae6da8adfbf1b35500ea19635779660
DariushJinx <106846875+DariushJinx@users.noreply.github.com>
2023-10-12 13:01:49 +0330

    file updated with adding array of images function

--- src/utils/functions.utils.ts
+++ src/utils/functions.utils.ts
@@ -1,5 +1,20 @@
+import { join } from 'path';
+
 export class FunctionUtils {
   public static RandomNumberGenerator(): number {
     return ~~(Math.random() * 90000 + 10000);
+  }
+
+  public static ListOfImagesForRequest(
+    files: Express.Multer.File[],
+    fileUploadPath: string,
+  ) {
+    if (files) {
+      return files
+        .map((file) => join('/', fileUploadPath, file.filename))
+        .map((item) => item.replace(/\\/g, '/'));
+    } else {
+      return [];
+    }
   }
 }
