6311c22621660cc42f754c94882954c38e0741f3
d.khazaei <www.dariushkhazaei629@gmail.com>
2023-11-07 09:59:15 +0330

    add:add route for category

--- src/category/category.controller.ts
+++ src/category/category.controller.ts
@@ -6,22 +6,25 @@ import {
   UseInterceptors,
   UsePipes,
   Get,
+  Delete,
   Param,
   Res,
 } from '@nestjs/common';
 import { CategoryService } from './category.service';
-import { BackendValidationPipe } from 'src/shared/pipes/backendValidation.pipe';
+import { BackendValidationPipe } from '../shared/pipes/backendValidation.pipe';
 import { CreateCategoryDto } from './dto/category.dto';
 import { FilesInterceptor } from '@nestjs/platform-express';
 import { multerConfig } from './middlewares/multer';
 import { of } from 'rxjs';
 import { join } from 'path';
+import { CategoriesResponseInterface } from './types/categoriesResponse.interface';
+import { DeleteResult } from 'typeorm';
 
-@Controller()
+@Controller('category')
 export class CategoryController {
   constructor(private readonly categoryService: CategoryService) {}
 
-  @Post('category/add')
+  @Post('add')
   @UsePipes(new BackendValidationPipe())
   @UseInterceptors(FilesInterceptor('images', 10, multerConfig))
   async addCategory(
@@ -30,6 +33,16 @@ export class CategoryController {
   ) {
     const category = await this.categoryService.addCategory(categoryDto, files);
     return this.categoryService.buildCategoryResponse(category);
+  }
+
+  @Get('list')
+  async getListOfCategories(): Promise<CategoriesResponseInterface> {
+    return this.categoryService.getListOfCategories();
+  }
+
+  @Delete(':id')
+  async deleteCategoryWithId(@Param('id') id: number): Promise<DeleteResult> {
+    return await this.categoryService.deleteCategory(id);
   }
 
   @Get(':imageName')
