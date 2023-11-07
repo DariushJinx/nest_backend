360ad40ddc2ddafe2d99f9dbcdce86a5e08fbe75
d.khazaei <www.dariushkhazaei629@gmail.com>
2023-11-07 09:57:46 +0330

    update:file updated with add user entity

--- src/blog/blog.module.ts
+++ src/blog/blog.module.ts
@@ -3,9 +3,10 @@ import { BlogController } from './blog.controller';
 import { BlogService } from './blog.service';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { BlogEntity } from './blog.entity';
+import { UserEntity } from '../user/user.entity';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([BlogEntity])],
+  imports: [TypeOrmModule.forFeature([BlogEntity, UserEntity])],
   controllers: [BlogController],
   providers: [BlogService],
 })
