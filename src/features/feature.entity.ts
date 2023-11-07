b2f34f3e31cd09cd7e7199945b47dde72069e95e
d.khazaei <www.dariushkhazaei629@gmail.com>
2023-11-07 10:05:58 +0330

    add:handle entity for features

---
+++ src/features/feature.entity.ts
@@ -0,0 +1,19 @@
+import { Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
+
+@Entity({ name: 'features' })
+export class FeatureEntity {
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @Column()
+  title: string;
+
+  @Column()
+  description: string;
+
+  @Column({ type: 'timestamp', default: () => 'CURRENT_TIMESTAMP' })
+  createdAt: Date;
+
+  @Column({ type: 'timestamp', default: () => 'CURRENT_TIMESTAMP' })
+  updatedAt: Date;
+}
