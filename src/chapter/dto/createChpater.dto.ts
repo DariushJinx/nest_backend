4ccf28af225121e7bf7a3d0d0c7e64cff355dacc
d.khazaei <www.dariushkhazaei629@gmail.com>
2023-11-09 19:38:17 +0330

    add:handle dto for create chapter

---
+++ src/chapter/dto/createChpater.dto.ts
@@ -0,0 +1,12 @@
+import { Expose } from 'class-transformer';
+import { IsDefined, Length } from 'class-validator';
+
+export class CreateChapterDto {
+  @IsDefined({ message: 'عنوان فصل یافت نشد' })
+  @Expose()
+  @Length(3, 30, { message: 'عنوان وارد شده صحیح نمی باشد' })
+  title: string;
+  @IsDefined({ message: 'متن فصل یافت نشد' })
+  @Expose()
+  text: string;
+}
