5525b1c69c1bd93ab7e1093c667a25148cf8bd8a
d.khazaei <www.dariushkhazaei629@gmail.com>
2023-11-10 15:49:19 +0330

    update: use chapter entity

--- src/course/course.module.ts
+++ src/course/course.module.ts
@@ -1,12 +1,15 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { CourseEntity } from './course.entity';
-import { UserEntity } from 'src/user/user.entity';
+import { UserEntity } from '../user/user.entity';
 import { CourseController } from './course.controller';
 import { CourseService } from './course.service';
+import { ChapterEntity } from '../chapter/chapter.entity';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([CourseEntity, UserEntity])],
+  imports: [
+    TypeOrmModule.forFeature([CourseEntity, UserEntity, ChapterEntity]),
+  ],
   controllers: [CourseController],
   providers: [CourseService],
 })
