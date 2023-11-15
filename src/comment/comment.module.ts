27b514e0c0e361c8f6607c63dbad048d5e6bc6a6
d.khazaei <www.dariushkhazaei629@gmail.com>
2023-11-15 21:29:30 +0330

    add:handle comment module

---
+++ src/comment/comment.module.ts
@@ -0,0 +1,22 @@
+import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { UserEntity } from '../user/user.entity';
+import { CourseEntity_2 } from '../course_2/course_2.entity';
+import { CommentController } from './comment.controller';
+import { CommentService } from './comment.service';
+import { ProductEntity } from '../product/product.entity';
+import { BlogEntity } from '../blog/blog.entity';
+
+@Module({
+  imports: [
+    TypeOrmModule.forFeature([
+      UserEntity,
+      CourseEntity_2,
+      ProductEntity,
+      BlogEntity,
+    ]),
+  ],
+  controllers: [CommentController],
+  providers: [CommentService],
+})
+export class CommentModule {}
