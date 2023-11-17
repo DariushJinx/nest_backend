6b32e7b9bd5057ca298518c385b95770bd435353
d.khazaei <www.dariushkhazaei629@gmail.com>
2023-11-17 22:58:58 +0330

    update:use comment entity

--- src/blog/blog.module.ts
+++ src/blog/blog.module.ts
@@ -4,9 +4,10 @@ import { BlogService } from './blog.service';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { BlogEntity } from './blog.entity';
 import { UserEntity } from '../user/user.entity';
+import { CommentEntity } from '../comment/comment.entity';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([BlogEntity, UserEntity])],
+  imports: [TypeOrmModule.forFeature([BlogEntity, UserEntity, CommentEntity])],
   controllers: [BlogController],
   providers: [BlogService],
 })
