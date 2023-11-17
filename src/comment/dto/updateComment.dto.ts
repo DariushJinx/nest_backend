054bc2b28b15237d13832ec7836b1e6f314ba404
d.khazaei <www.dariushkhazaei629@gmail.com>
2023-11-17 23:10:04 +0330

    update:handle update dto

--- src/comment/dto/updateComment.dto.ts
+++ src/comment/dto/updateComment.dto.ts
@@ -1,8 +1,4 @@
-import { Expose } from 'class-transformer';
-import { IsOptional } from 'class-validator';
 export class UpdateCommentDto {
   readonly comment: string;
-  @IsOptional()
-  @Expose()
   readonly score: number;
 }
