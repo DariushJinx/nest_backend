a79f9df73a3385b50e492d5395775e8ae592fdc5
d.khazaei <www.dariushkhazaei629@gmail.com>
2023-11-20 22:25:53 +0330

    add:handle contact entity

---
+++ src/contact/contact.entity.ts
@@ -0,0 +1,28 @@
+import { Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
+
+@Entity({ name: 'contact' })
+export class ContactEntity {
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @Column()
+  name: string;
+
+  @Column()
+  email: string;
+
+  @Column()
+  phone: string;
+
+  @Column({ default: false })
+  answer: boolean;
+
+  @Column()
+  body: string;
+
+  @Column({ type: 'timestamp', default: () => 'CURRENT_TIMESTAMP' })
+  createdAt: Date;
+
+  @Column({ type: 'timestamp', default: () => 'CURRENT_TIMESTAMP' })
+  updatedAt: Date;
+}
