7f64d789d6c613bbb247da68f99deb99ae99ada5
d.khazaei <www.dariushkhazaei629@gmail.com>
2023-11-20 22:26:43 +0330

    add:handle contact module

---
+++ src/contact/contact.module.ts
@@ -0,0 +1,13 @@
+import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { ContactEntity } from './contact.entity';
+import { ContactController } from './contact.controller';
+import { ContactService } from './contact.service';
+import { UserEntity } from 'src/user/user.entity';
+
+@Module({
+  imports: [TypeOrmModule.forFeature([ContactEntity, UserEntity])],
+  controllers: [ContactController],
+  providers: [ContactService],
+})
+export class ContactModule {}
