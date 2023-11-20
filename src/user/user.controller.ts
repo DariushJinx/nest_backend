51c4bdfab922f10b96f5db6104ea3f4d555383ef
d.khazaei <www.dariushkhazaei629@gmail.com>
2023-11-20 22:29:22 +0330

    update handle ban route and use in another route

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,26 +1,47 @@
-import { Body, Controller, Post, UsePipes } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Param,
+  Post,
+  Put,
+  UseGuards,
+  UsePipes,
+} from '@nestjs/common';
 import { UserService } from './user.service';
 import { RegisterDto } from './dto/register.dto';
 import { UserType } from './types/user.types';
 import { BackendValidationPipe } from 'src/shared/pipes/backendValidation.pipe';
 import { LoginDto } from './dto/login.dto';
 import { UserResponseInterface } from './types/userResponse.interface';
+import { AuthGuard } from './guard/auth.guard';
+import { User } from 'src/decorators/user.decorators';
+import { UserEntity } from './user.entity';
 
-@Controller('auth')
+@Controller('')
 export class UserController {
   constructor(private readonly userService: UserService) {}
 
-  @Post('register')
+  @Post('auth/register')
   @UsePipes(new BackendValidationPipe())
   async registerUser(@Body() registerDto: RegisterDto): Promise<UserType> {
     const user = await this.userService.registerUser(registerDto);
     return user;
   }
 
-  @Post('login')
+  @Post('auth/login')
   @UsePipes(new BackendValidationPipe())
   async loginUser(@Body() loginDto: LoginDto): Promise<UserResponseInterface> {
     const user = await this.userService.loginUser(loginDto);
+    return await this.userService.buildUserResponse(user);
+  }
+
+  @Put('user/ban/:id')
+  @UseGuards(AuthGuard)
+  async followProfile(
+    @User() currentUser: UserEntity,
+    @Param('id') id: number,
+  ) {
+    const user = await this.userService.banUser(currentUser, id);
     return await this.userService.buildUserResponse(user);
   }
 }
