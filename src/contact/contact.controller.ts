4c376fc61dc510d4c89a17494a76777900836696
d.khazaei <www.dariushkhazaei629@gmail.com>
2023-11-20 22:25:39 +0330

    add:handle contact controller

---
+++ src/contact/contact.controller.ts
@@ -0,0 +1,69 @@
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  Param,
+  Post,
+  Query,
+  UseGuards,
+  UsePipes,
+} from '@nestjs/common';
+import { ContactService } from './contact.service';
+import { AuthGuard } from 'src/user/guard/auth.guard';
+import { BackendValidationPipe } from 'src/shared/pipes/backendValidation.pipe';
+import { User } from 'src/decorators/user.decorators';
+import { UserEntity } from 'src/user/user.entity';
+import { CreateContactDto } from './dto/createContact.dto';
+import { ContactsResponseInterface } from './types/contactsResponse.interface';
+import { DeleteResult } from 'typeorm';
+import { CreateAnswerDto } from './dto/createAnswer.dto';
+
+@Controller('contact')
+export class ContactController {
+  constructor(private readonly contactService: ContactService) {}
+
+  @Post('add')
+  @UseGuards(AuthGuard)
+  @UsePipes(new BackendValidationPipe())
+  async createContact(
+    @User() currentUser: UserEntity,
+    @Body() createContactDto: CreateContactDto,
+  ) {
+    const contact = await this.contactService.createContact(
+      currentUser,
+      createContactDto,
+    );
+    return await this.contactService.buildContactResponse(contact);
+  }
+
+  @Post('answer')
+  @UseGuards(AuthGuard)
+  @UsePipes(new BackendValidationPipe())
+  async createAnswer(
+    @User() currentUser: UserEntity,
+    @Body() createAnswerDto: CreateAnswerDto,
+  ) {
+    await this.contactService.createAnswer(currentUser, createAnswerDto);
+    return {
+      message: 'ثبت پاسخ موفقیت آمیز بود',
+    };
+  }
+
+  @Get('list')
+  async findAllContacts(
+    @User() currentUser: number,
+    @Query() query: any,
+  ): Promise<ContactsResponseInterface> {
+    return await this.contactService.findAllContacts(currentUser, query);
+  }
+
+  @Delete(':id')
+  @UseGuards(AuthGuard)
+  async deleteOneContact(
+    @Param('id') id: number,
+    @User() currentUser: UserEntity,
+  ): Promise<DeleteResult> {
+    return await this.contactService.deleteOneContact(id, currentUser);
+  }
+}
