1f3b56cd5fa5d0dcc5ecda8696e461c2565dcb2c
Dariush Khazaei <dariushkhazaei629@gmail.com>
2023-11-22 16:54:29 +0330

    use headers token

--- src/user/guard/auth.guard.ts
+++ src/user/guard/auth.guard.ts
@@ -11,9 +11,9 @@ import { ExpressRequest } from 'src/types/expressRequest.interface';
 export class AuthGuard implements CanActivate {
   canActivate(context: ExecutionContext): boolean {
     const request = context.switchToHttp().getRequest<ExpressRequest>();
-    if (request.user) {
+    if (request.headers.token) {
       return true;
     }
-    throw new HttpException('Not Authorized', HttpStatus.UNAUTHORIZED);
+    throw new HttpException('شما مجاز نیستید', HttpStatus.UNAUTHORIZED);
   }
 }
