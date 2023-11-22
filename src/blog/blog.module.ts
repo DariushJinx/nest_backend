f2c8045ce25f89024de853091998be29f443934c
Dariush Khazaei <dariushkhazaei629@gmail.com>
2023-11-22 16:53:56 +0330

    use AdminAuthMiddleware

--- src/blog/blog.module.ts
+++ src/blog/blog.module.ts
@@ -1,14 +1,36 @@
-import { Module } from '@nestjs/common';
+import {
+  MiddlewareConsumer,
+  Module,
+  NestModule,
+  RequestMethod,
+} from '@nestjs/common';
 import { BlogController } from './blog.controller';
 import { BlogService } from './blog.service';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { BlogEntity } from './blog.entity';
 import { UserEntity } from '../user/user.entity';
 import { CommentEntity } from '../comment/comment.entity';
+import { AdminEntity } from '../admin/admin.entity';
+import { AdminAuthMiddleware } from 'src/admin/middlewares/auth.admin.middleware';
+import { AdminService } from '../admin/admin.service';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([BlogEntity, UserEntity, CommentEntity])],
+  imports: [
+    TypeOrmModule.forFeature([
+      BlogEntity,
+      UserEntity,
+      CommentEntity,
+      AdminEntity,
+    ]),
+  ],
   controllers: [BlogController],
-  providers: [BlogService],
+  providers: [BlogService, AdminService],
 })
-export class BlogModule {}
+export class BlogModule implements NestModule {
+  configure(consumer: MiddlewareConsumer) {
+    consumer.apply(AdminAuthMiddleware).forRoutes({
+      path: '*',
+      method: RequestMethod.ALL,
+    });
+  }
+}
