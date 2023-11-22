ca2d025922f8927ff5b6df44d175fc9b1b2cdc73
Dariush Khazaei <dariushkhazaei629@gmail.com>
2023-11-22 14:50:38 +0330

    add:handle auth admin middleware

---
+++ src/admin/middlewares/auth.admin.middleware.ts
@@ -0,0 +1,58 @@
+import {
+  HttpException,
+  HttpStatus,
+  Injectable,
+  NestMiddleware,
+} from '@nestjs/common';
+import { NextFunction, Response } from 'express';
+import { verify } from 'jsonwebtoken';
+import { ACCESS_TOKEN_SECRET_KEY } from 'src/config';
+import { AdminService } from '../admin.service';
+import { ExpressAdminRequest } from '../../types/expressAdminRequest.interface copy';
+
+@Injectable()
+export class AdminAuthMiddleware implements NestMiddleware {
+  constructor(private readonly adminService: AdminService) {}
+
+  async use(req: ExpressAdminRequest, res: Response, next: NextFunction) {
+    if (!req.headers.adminToken) {
+      req.admin = null;
+      next();
+      return;
+    }
+
+    let token: any;
+    if (req.headers && req.headers.adminToken) {
+      token = req.headers.adminToken;
+    }
+
+    try {
+      verify(token, ACCESS_TOKEN_SECRET_KEY, async (err: any, payload: any) => {
+        try {
+          if (err) {
+            throw new HttpException(
+              'وارد حساب کاربری خود شوید...',
+              HttpStatus.UNAUTHORIZED,
+            );
+          } else {
+            const { email } = payload;
+            const admin = await this.adminService.findAdminByEmail(email);
+            if (!admin) {
+              throw new HttpException(
+                'وارد حساب کاربری خود شوید...',
+                HttpStatus.UNAUTHORIZED,
+              );
+            }
+            req.admin = admin;
+            return next();
+          }
+        } catch (err) {
+          next(err);
+        }
+      });
+    } catch (err) {
+      req.admin = null;
+      next(err);
+    }
+  }
+}
