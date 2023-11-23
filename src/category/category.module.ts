4862e8565f54873e267ba2f1de5d04f96f6cf8e3
Dariush Khazaei <dariushkhazaei629@gmail.com>
2023-11-23 14:24:37 +0330

    handle things to use admin auth middleware

--- src/category/category.module.ts
+++ src/category/category.module.ts
@@ -1,12 +1,27 @@
-import { Module } from '@nestjs/common';
+import {
+  Module,
+  NestModule,
+  MiddlewareConsumer,
+  RequestMethod,
+} from '@nestjs/common';
 import { CategoryController } from './category.controller';
 import { CategoryService } from './category.service';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { CategoryEntity } from './category.entity';
+import { AdminEntity } from '../admin/admin.entity';
+import { AdminAuthMiddleware } from '../admin/middlewares/auth.admin.middleware';
+import { AdminService } from '../admin/admin.service';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([CategoryEntity])],
+  imports: [TypeOrmModule.forFeature([CategoryEntity, AdminEntity])],
   controllers: [CategoryController],
-  providers: [CategoryService],
+  providers: [CategoryService, AdminService],
 })
-export class CategoryModule {}
+export class CategoryModule implements NestModule {
+  configure(consumer: MiddlewareConsumer) {
+    consumer.apply(AdminAuthMiddleware).forRoutes({
+      path: '*',
+      method: RequestMethod.ALL,
+    });
+  }
+}
