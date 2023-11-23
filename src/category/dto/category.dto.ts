36257b58075d51bba9da3fdf86be38e641b5afef
d.khazaei <www.dariushkhazaei629@gmail.com>
2023-11-23 20:21:36 +0330

    update category dto

--- src/category/dto/category.dto.ts
+++ src/category/dto/category.dto.ts
@@ -1,24 +1,22 @@
 import { Expose } from 'class-transformer';
 import { IsDefined, Matches, Length, Allow, IsOptional } from 'class-validator';
-import { MongoIDPattern } from 'src/config';
 export class CreateCategoryDto {
   @IsDefined()
   @Expose()
   @Length(3, 30, { message: 'عنوان دسته بندی وارد شده صحیح نمی باشد' })
   title: string;
-  @Expose()
-  @Allow()
   @IsOptional()
-  @Matches(MongoIDPattern, {
-    message: 'شناسه وارد شده صحیح نمی باشد',
-  })
-  parent?: string;
+  @Expose()
+  parent: number;
+  @IsOptional()
+  @Expose()
+  tree_comment: string[];
   @Expose()
   @IsOptional()
   @Matches(/(\.png|\.jpg|\.webp|\.jpeg|\.gif)$/, {
     message: 'فرمت عکس ارسالی صحیح نمی باشد',
   })
-  filename?: string;
+  filename: string;
   @Allow()
   fileUploadPath: string;
 }
