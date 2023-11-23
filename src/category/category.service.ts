8ba959a4ce7cd4e627c22ae8cf65b3f61eace18b
d.khazaei <www.dariushkhazaei629@gmail.com>
2023-11-23 20:21:26 +0330

    handle many routes in category service

--- src/category/category.service.ts
+++ src/category/category.service.ts
@@ -1,18 +1,21 @@
 import { HttpException, HttpStatus, Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
-import { DeleteResult, Repository } from 'typeorm';
+import { Repository } from 'typeorm';
 import { CategoryEntity } from './category.entity';
 import { CreateCategoryDto } from './dto/category.dto';
 import { CategoryResponseInterface } from './types/categoryResponse.interface';
 import { FunctionUtils } from '../utils/functions.utils';
 import { CategoriesResponseInterface } from './types/categoriesResponse.interface';
 import { AdminEntity } from '../admin/admin.entity';
+import { UpdateCategoryDto } from './dto/updateCategory.dto';
 
 @Injectable()
 export class CategoryService {
   constructor(
     @InjectRepository(CategoryEntity)
     private readonly categoryRepository: Repository<CategoryEntity>,
+    @InjectRepository(AdminEntity)
+    private readonly adminRepository: Repository<AdminEntity>,
   ) {}
 
   async addCategory(
@@ -35,6 +38,11 @@ export class CategoryService {
     delete categoryDto.filename;
     Object.assign(category, categoryDto);
 
+    category.tree_comment = [];
+    category.register = admin;
+    delete category.register.password;
+    category.images = images;
+
     const checkExistCategory = await this.categoryRepository.findOne({
       where: {
         title: category.title,
@@ -46,37 +54,138 @@ export class CategoryService {
         'دسته بندی وارد شده از قبل موجود می باشد';
       throw new HttpException(errorResponse, HttpStatus.BAD_REQUEST);
     }
+    const saveCategory = await this.categoryRepository.save(category);
 
-    category.register = admin;
-    delete category.register.password;
-    category.images = images;
-    return await this.categoryRepository.save(category);
+    const categories = await this.categoryRepository.find();
+
+    categories.forEach(async (comment: any) => {
+      if (category.parent && category.parent !== 0) {
+        let parentCode = category.parent.toString();
+        category.tree_comment = [category.id.toString(), parentCode];
+        let parent = categories.find(
+          (item) => item.id.toString() === parentCode,
+        );
+        while (parent && parent.parent !== 0) {
+          parentCode = parent.parent.toString();
+          category.tree_comment.push(parentCode);
+          parent = categories.find((item) => item.id.toString() === parentCode);
+        }
+      } else {
+        category.tree_comment = [category.id.toString()];
+      }
+      await this.categoryRepository.update(
+        { id: comment.id },
+        { tree_comment: comment.tree_comment },
+      );
+      await this.categoryRepository.save(saveCategory);
+    });
+
+    return saveCategory;
   }
 
-  async getListOfCategories(): Promise<CategoriesResponseInterface> {
-    const queryBuilder =
-      this.categoryRepository.createQueryBuilder('categories');
+  async getListOfCategories(query: any): Promise<CategoriesResponseInterface> {
+    const queryBuilder = this.categoryRepository
+      .createQueryBuilder('category')
+      .leftJoinAndSelect('category.register', 'register');
+
+    if (query.register) {
+      const register = await this.adminRepository.findOne({
+        where: { username: query.register },
+      });
+      if (!register) {
+        throw new HttpException(
+          'دسته بندی ای با این نویسنده یافت نشد',
+          HttpStatus.NOT_FOUND,
+        );
+      }
+      queryBuilder.andWhere('category.registerId = :id', {
+        id: register.id,
+      });
+    }
+
+    if (query.limit) {
+      queryBuilder.limit(query.limit);
+    }
+
+    if (query.offset) {
+      queryBuilder.offset(query.offset);
+    }
+
+    queryBuilder.orderBy('category.createdAt', 'DESC');
 
     const categoriesCount = await queryBuilder.getCount();
-    const categories = await queryBuilder.getMany();
-    return { categories, categoriesCount };
+    const allCategories = await queryBuilder.getMany();
+
+    if (!allCategories.length) {
+      throw new HttpException('مقاله ای یافت نشد', HttpStatus.NOT_FOUND);
+    }
+
+    allCategories.forEach((category) => {
+      delete category.register.password;
+    });
+    return { allCategories, categoriesCount };
   }
 
   async getOneCategory(id: number) {
-    return await this.categoryRepository.findOne({
+    const category = await this.categoryRepository.findOne({
       where: { id: id },
     });
-  }
 
-  async deleteCategory(id: number): Promise<DeleteResult> {
-    const category = await this.getOneCategory(id);
     if (!category) {
       throw new HttpException(
         'دسته بندی مورد نظر یافت نشد',
         HttpStatus.NOT_FOUND,
       );
     }
-    return await this.categoryRepository.delete({ id });
+
+    delete category.register.password;
+
+    return category;
+  }
+
+  async deleteCategory(
+    id: number,
+    admin: AdminEntity,
+  ): Promise<{ message: string }> {
+    await this.getOneCategory(id);
+
+    if (!admin) {
+      throw new HttpException(
+        'شما مجاز به حذف دسته بندی نیستید',
+        HttpStatus.FORBIDDEN,
+      );
+    }
+
+    await this.categoryRepository.delete({ id });
+
+    return {
+      message: 'دسته بندی مورد نظر با موفقیت حذف گردید',
+    };
+  }
+
+  async updateCategory(
+    id: number,
+    admin: AdminEntity,
+    updateCategoryDto: UpdateCategoryDto,
+  ) {
+    const errorResponse = {
+      errors: {},
+    };
+
+    const category = await this.getOneCategory(id);
+
+    if (!admin) {
+      errorResponse.errors['admin'] =
+        'شما مجاز به به روز رسانی دسته بندی نیستید';
+      errorResponse.errors['statusCode'] = HttpStatus.FORBIDDEN;
+      throw new HttpException(errorResponse, HttpStatus.FORBIDDEN);
+    }
+
+    Object.assign(category, updateCategoryDto);
+
+    delete category.register.password;
+
+    return await this.categoryRepository.save(category);
   }
 
   async buildCategoryResponse(
