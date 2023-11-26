814953e0b9704d18a6a4b73bdc97c4e471ded702
d.khazaei <www.dariushkhazaei629@gmail.com>
2023-11-26 22:17:30 +0330

    add:handle product category entity

---
+++ src/productCategory/productCategory.entity.ts
@@ -0,0 +1,42 @@
+import { ProductEntity } from '../product/product.entity';
+import {
+  Column,
+  Entity,
+  ManyToOne,
+  OneToMany,
+  PrimaryGeneratedColumn,
+} from 'typeorm';
+import { AdminEntity } from '../admin/admin.entity';
+
+@Entity({ name: 'productCategory' })
+export class ProductCategoryEntity {
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @Column()
+  title: string;
+
+  @Column('simple-array')
+  images: string[];
+
+  @OneToMany(() => ProductEntity, (product) => product.category)
+  products: ProductEntity[];
+
+  @ManyToOne(() => AdminEntity, (admin) => admin.categories, { eager: true })
+  register: AdminEntity;
+
+  @Column({ default: 0 })
+  parent: number;
+
+  @Column({ default: 0 })
+  isLast: number;
+
+  @Column('simple-array')
+  tree_cat: string[];
+
+  @Column({ type: 'timestamp', default: () => 'CURRENT_TIMESTAMP' })
+  createdAt: Date;
+
+  @Column({ type: 'timestamp', default: () => 'CURRENT_TIMESTAMP' })
+  updatedAt: Date;
+}
