73a824f313df0ac77565cf17d98293a7b21ac8bb
Dariush Khazaei <dariushkhazaei629@gmail.com>
2023-11-29 15:26:16 +0330

    update chapter with adding episode entity

--- src/chapter_2/chapter_2.module.ts
+++ src/chapter_2/chapter_2.module.ts
@@ -1,11 +1,11 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { UserEntity } from '../user/user.entity';
-import { EpisodeEntity } from 'src/episode/episode.entity';
 import { ChapterService_2 } from './chpater_2.service';
 import { ChapterEntity_2 } from './chapter_2.entity';
 import { ChapterController_2 } from './chapter_2.controller';
 import { CourseEntity_2 } from 'src/course_2/course_2.entity';
+import { EpisodeEntity_2 } from 'src/episode_2/episode_2.entity';
 
 @Module({
   imports: [
@@ -13,7 +13,7 @@ import { CourseEntity_2 } from 'src/course_2/course_2.entity';
       ChapterEntity_2,
       UserEntity,
       CourseEntity_2,
-      EpisodeEntity,
+      EpisodeEntity_2,
     ]),
   ],
   controllers: [ChapterController_2],
