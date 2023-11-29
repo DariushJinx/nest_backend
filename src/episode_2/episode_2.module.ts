f14fa201edb26e784d34ed464c09e6932d671b3f
Dariush Khazaei <dariushkhazaei629@gmail.com>
2023-11-29 15:30:01 +0330

    remove course entity from EpisodeModule_2

--- src/episode_2/episode_2.module.ts
+++ src/episode_2/episode_2.module.ts
@@ -1,18 +1,18 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { UserEntity } from '../user/user.entity';
-import { CourseEntity } from '../course/course.entity';
 import { EpisodeEntity_2 } from './episode_2.entity';
 import { EpisodeController_2 } from './episode_2.controller';
 import { EpisodeService_2 } from './episode_2.service';
 import { ChapterEntity_2 } from '../chapter_2/chapter_2.entity';
+import { CourseEntity_2 } from 'src/course_2/course_2.entity';
 
 @Module({
   imports: [
     TypeOrmModule.forFeature([
       EpisodeEntity_2,
       UserEntity,
-      CourseEntity,
+      CourseEntity_2,
       ChapterEntity_2,
     ]),
   ],
