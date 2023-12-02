9dcbffda5e533114be8136ea81b55da6728ed2c5
Dariush Khazaei <dariushkhazaei629@gmail.com>
2023-12-02 16:39:59 +0330

    use adminEntity/set AdminAuthMiddleware

--- src/user/user.module.ts
+++ src/user/user.module.ts
@@ -1,14 +1,28 @@
-import { Module } from '@nestjs/common';
+import {
+  Module,
+  NestModule,
+  MiddlewareConsumer,
+  RequestMethod,
+} from '@nestjs/common';
 import { UserController } from './user.controller';
 import { UserService } from './user.service';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { UserEntity } from './user.entity';
-import { AuthGuard } from './guard/auth.guard';
+import { AdminService } from '../admin/admin.service';
+import { AdminEntity } from '../admin/admin.entity';
+import { AdminAuthMiddleware } from '../admin/middlewares/auth.admin.middleware';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([UserEntity])],
+  imports: [TypeOrmModule.forFeature([UserEntity, AdminEntity])],
   controllers: [UserController],
-  providers: [UserService, AuthGuard],
+  providers: [UserService, AdminService],
   exports: [UserService],
 })
-export class UserModule {}
+export class UserModule implements NestModule {
+  configure(consumer: MiddlewareConsumer) {
+    consumer.apply(AdminAuthMiddleware).forRoutes({
+      path: '*',
+      method: RequestMethod.ALL,
+    });
+  }
+}
