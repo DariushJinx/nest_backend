00c9cd1d69c2dcac82d2d62189ee276a1ac8e157
Dariush Khazaei <dariushkhazaei629@gmail.com>
2023-12-03 16:43:24 +0330

    use AdminAuthMiddleware in episode module

--- src/episode_2/episode_2.module.ts
+++ src/episode_2/episode_2.module.ts
@@ -1,11 +1,19 @@
-import { Module } from '@nestjs/common';
+import {
+  MiddlewareConsumer,
+  Module,
+  NestModule,
+  RequestMethod,
+} from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { UserEntity } from '../user/user.entity';
 import { EpisodeEntity_2 } from './episode_2.entity';
 import { EpisodeController_2 } from './episode_2.controller';
 import { EpisodeService_2 } from './episode_2.service';
 import { ChapterEntity_2 } from '../chapter_2/chapter_2.entity';
-import { CourseEntity_2 } from 'src/course_2/course_2.entity';
+import { CourseEntity_2 } from '../course_2/course_2.entity';
+import { AdminAuthMiddleware } from '../admin/middlewares/auth.admin.middleware';
+import { AdminService } from '../admin/admin.service';
+import { AdminEntity } from '../admin/admin.entity';
 
 @Module({
   imports: [
@@ -14,9 +22,17 @@ import { CourseEntity_2 } from 'src/course_2/course_2.entity';
       UserEntity,
       CourseEntity_2,
       ChapterEntity_2,
+      AdminEntity,
     ]),
   ],
   controllers: [EpisodeController_2],
-  providers: [EpisodeService_2],
+  providers: [EpisodeService_2, AdminService],
 })
-export class EpisodeModule_2 {}
+export class EpisodeModule_2 implements NestModule {
+  configure(consumer: MiddlewareConsumer) {
+    consumer.apply(AdminAuthMiddleware).forRoutes({
+      path: '*',
+      method: RequestMethod.ALL,
+    });
+  }
+}
