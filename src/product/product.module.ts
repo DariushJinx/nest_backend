884090444b2abfa34478507b0e2c3cef0957d285
Dariush Khazaei <dariushkhazaei629@gmail.com>
2023-12-03 16:45:41 +0330

    use AdminAuthMiddleware in product module

--- src/product/product.module.ts
+++ src/product/product.module.ts
@@ -1,4 +1,9 @@
-import { Module } from '@nestjs/common';
+import {
+  MiddlewareConsumer,
+  Module,
+  NestModule,
+  RequestMethod,
+} from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { ProductController } from './product.controller';
 import { ProductService } from './product.service';
@@ -6,19 +11,30 @@ import { ProductEntity } from './product.entity';
 import { UserEntity } from '../user/user.entity';
 import { FeatureEntity } from '../features/feature.entity';
 import { CommentEntity } from '../comment/comment.entity';
-import { ProductCategoryEntity } from 'src/productCategory/productCategory.entity';
+import { ProductCategoryEntity } from '../productCategory/productCategory.entity';
+import { AdminAuthMiddleware } from '../admin/middlewares/auth.admin.middleware';
+import { AdminEntity } from '../admin/admin.entity';
+import { AdminService } from '../admin/admin.service';
 
 @Module({
   imports: [
     TypeOrmModule.forFeature([
       ProductEntity,
       UserEntity,
+      AdminEntity,
       FeatureEntity,
       CommentEntity,
       ProductCategoryEntity,
     ]),
   ],
   controllers: [ProductController],
-  providers: [ProductService],
+  providers: [ProductService, AdminService],
 })
-export class ProductModule {}
+export class ProductModule implements NestModule {
+  configure(consumer: MiddlewareConsumer) {
+    consumer.apply(AdminAuthMiddleware).forRoutes({
+      path: '*',
+      method: RequestMethod.ALL,
+    });
+  }
+}
