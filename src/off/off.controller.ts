f254e729801f063cf4efd02e056a3e9c0bcd546c
Dariush Khazaei <dariushkhazaei629@gmail.com>
2023-12-06 16:54:07 +0330

    use admin and update routes in off controller

--- src/off/off.controller.ts
+++ src/off/off.controller.ts
@@ -10,10 +10,7 @@ import {
   Put,
   Delete,
 } from '@nestjs/common';
-import { AuthGuard } from 'src/user/guard/auth.guard';
 import { BackendValidationPipe } from 'src/shared/pipes/backendValidation.pipe';
-import { User } from 'src/decorators/user.decorators';
-import { UserEntity } from 'src/user/user.entity';
 import { CreateOffDto } from './dto/createOff.dto';
 import { OffService } from './off.service';
 import { offsResponseInterface } from './types/offsResponse.interface';
@@ -22,29 +19,33 @@ import { UpdateOffDto } from './dto/updateOff.dto';
 import { DeleteResult } from 'typeorm';
 import { GetOneOffDto } from './dto/getOneOff.dto';
 import { SetDiscountOnAllDto } from './dto/setOffAll.dto';
+import { AdminAuthGuard } from 'src/admin/guard/adminAuth.guard';
+import { Admin } from 'src/decorators/admin.decorators';
+import { AdminEntity } from 'src/admin/admin.entity';
 
 @Controller('off')
 export class OffController {
   constructor(private readonly offService: OffService) {}
 
   @Post('add')
-  @UseGuards(AuthGuard)
+  @UseGuards(AdminAuthGuard)
   @UsePipes(new BackendValidationPipe())
-  async createEpisode(
-    @User() user: UserEntity,
+  async createOff(
+    @Admin() admin: AdminEntity,
     @Body() createOffDto: CreateOffDto,
   ) {
-    const off = await this.offService.createOff(user, createOffDto);
+    const off = await this.offService.createOff(admin, createOffDto);
     return await this.offService.buildOffResponse(off);
   }
 
   @Post('setAllCourses')
-  @UseGuards(AuthGuard)
+  @UseGuards(AdminAuthGuard)
   @UsePipes(new BackendValidationPipe())
   async setOffForAllCourses(
     @Body('') setDiscountOnAllDto: SetDiscountOnAllDto,
+    @Admin() admin: AdminEntity,
   ) {
-    await this.offService.setOffForAllCourses(setDiscountOnAllDto);
+    await this.offService.setOffForAllCourses(setDiscountOnAllDto, admin);
 
     return {
       message: 'تخفیف روی تمامی دوره ها ثبت شد',
@@ -52,23 +53,23 @@ export class OffController {
   }
 
   @Post('setAllProducts')
-  async setOff(@Body('') setDiscount: SetDiscountOnAllDto) {
-    await this.offService.setOffForAllProducts(setDiscount);
+  async setOff(
+    @Body('') setDiscount: SetDiscountOnAllDto,
+    @Admin() admin: AdminEntity,
+  ) {
+    await this.offService.setOffForAllProducts(setDiscount, admin);
     return {
       message: 'تخفیف روی تمامی محصولات ثبت شد',
     };
   }
 
   @Get('list')
-  async findAllOffs(
-    @User() currentUser: number,
-    @Query() query: any,
-  ): Promise<offsResponseInterface> {
-    return await this.offService.findAllOffs(currentUser, query);
+  async findAllOffs(@Query() query: any): Promise<offsResponseInterface> {
+    return await this.offService.findAllOffs(query);
   }
 
   @Get('product/:code')
-  async OffuseUsesForProduct(
+  async OffUseUsesForProduct(
     @Param('code') code: string,
     @Body() getOneOffDto: GetOneOffDto,
   ) {
@@ -77,7 +78,7 @@ export class OffController {
   }
 
   @Get('course/:code')
-  async OffuseUsesForCourse(
+  async OffUseUsesForCourse(
     @Param('code') code: string,
     @Body() getOneOffDto: GetOneOffDto,
   ) {
@@ -86,33 +87,29 @@ export class OffController {
   }
 
   @Get(':id')
-  async getOneChapter(@Param('id') id: number): Promise<OffResponseInterface> {
+  async getOneOff(@Param('id') id: number): Promise<OffResponseInterface> {
     const off = await this.offService.currentOff(id);
     return await this.offService.buildOffResponse(off);
   }
 
   @Put(':id')
-  @UseGuards(AuthGuard)
+  @UseGuards(AdminAuthGuard)
   @UsePipes(new BackendValidationPipe())
-  async updateOneChapterWithId(
+  async updateOneOffWithId(
     @Param('id') id: number,
-    @User('id') currentUserID: number,
-    @Body('') updateChapterDto: UpdateOffDto,
+    @Admin() admin: AdminEntity,
+    @Body('') updateOffDto: UpdateOffDto,
   ): Promise<OffResponseInterface> {
-    const off = await this.offService.updateOff(
-      id,
-      currentUserID,
-      updateChapterDto,
-    );
+    const off = await this.offService.updateOff(id, admin, updateOffDto);
     return await this.offService.buildOffResponse(off);
   }
 
   @Delete(':id')
-  @UseGuards(AuthGuard)
-  async deleteOneChapter(
+  @UseGuards(AdminAuthGuard)
+  async deleteOneOff(
     @Param('id') id: number,
-    @User() user: UserEntity,
+    @Admin() admin: AdminEntity,
   ): Promise<DeleteResult> {
-    return await this.offService.deleteOneOffWithID(id, user);
+    return await this.offService.deleteOneOffWithID(id, admin);
   }
 }
