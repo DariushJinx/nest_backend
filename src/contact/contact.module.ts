2c9ee6d0613a19c32f9d2331e60a431c707b9a18
Dariush Khazaei <dariushkhazaei629@gmail.com>
2023-12-06 16:53:13 +0330

    update routes in contact module

--- src/contact/contact.module.ts
+++ src/contact/contact.module.ts
@@ -3,7 +3,7 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { ContactEntity } from './contact.entity';
 import { ContactController } from './contact.controller';
 import { ContactService } from './contact.service';
-import { UserEntity } from 'src/user/user.entity';
+import { UserEntity } from '../user/user.entity';
 
 @Module({
   imports: [TypeOrmModule.forFeature([ContactEntity, UserEntity])],
