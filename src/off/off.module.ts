87a097366049fa98c0faafd782433e190d8703b6
Dariush Khazaei <dariushkhazaei629@gmail.com>
2023-12-06 16:55:03 +0330

    use AdminEntity and set AdminAuthMiddleware

--- src/off/off.module.ts
+++ src/off/off.module.ts
@@ -1,22 +1,36 @@
-import { Module } from '@nestjs/common';
+import {
+  Module,
+  NestModule,
+  MiddlewareConsumer,
+  RequestMethod,
+} from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { OffController } from './off.controller';
 import { OffService } from './off.service';
 import { OffEntity } from './off.entity';
-import { UserEntity } from 'src/user/user.entity';
-import { ProductEntity } from 'src/product/product.entity';
-import { CourseEntity_2 } from 'src/course_2/course_2.entity';
+import { ProductEntity } from '../product/product.entity';
+import { CourseEntity_2 } from '../course_2/course_2.entity';
+import { AdminEntity } from '../admin/admin.entity';
+import { AdminAuthMiddleware } from '../admin/middlewares/auth.admin.middleware';
+import { AdminService } from '../admin/admin.service';
 
 @Module({
   imports: [
     TypeOrmModule.forFeature([
       ProductEntity,
-      UserEntity,
       CourseEntity_2,
       OffEntity,
+      AdminEntity,
     ]),
   ],
   controllers: [OffController],
-  providers: [OffService],
+  providers: [OffService, AdminService],
 })
-export class OffModule {}
+export class OffModule implements NestModule {
+  configure(consumer: MiddlewareConsumer) {
+    consumer.apply(AdminAuthMiddleware).forRoutes({
+      path: '*',
+      method: RequestMethod.ALL,
+    });
+  }
+}
